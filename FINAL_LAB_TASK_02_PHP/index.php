09487eaf1ddbd8b9787382e0cabf5e247eedee3d
Anik <anikhossain18@gmail.com>
2021-03-27 22:37:13 +0600

    Update and search

---
+++ FINAL_LAB_TASK_02_PHP/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home Page</title>
+</head>
+<body>
+	<center>
+		<h2>
+		<a href="view/addProduct.html"> Add Product</a> ||
+		<a href="view/display.php"> Display Product </a>||
+		<a href="view/search.php">Search Product</a>
+		</h2>
+	</center>
+</body>
+</html>
